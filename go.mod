e779de8d43330c3292bafe30f22bf08a4e74be13
Artem V. Navrotskiy <bozaro@yandex.ru>
2020-02-09 23:51:59 +0300

    Sample `pull` command

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
+	github.com/heroku/docker-registry-client v0.0.0-20190909225348-afc9e1acc3d5
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
--- src/pull.go
+++ src/pull.go
@@ -1,7 +1,36 @@
 package src
 
-import "context"
+import (
+	"context"
+	"github.com/heroku/docker-registry-client/registry"
+	"io/ioutil"
+)
 
 func Pull(ctx context.Context, images ...string) error {
+	url := "https://registry-1.docker.io/"
+	username := "" // anonymous
+	password := "" // anonymous
+	hub, err := registry.New(url, username, password)
+	if err != nil {
+		return err
+	}
+
+	manifest, err := hub.Manifest("library/alpine", "latest")
+	if err != nil {
+		return err
+	}
+
+	for _, layer := range manifest.FSLayers {
+		reader, err := hub.DownloadBlob("library/alpine", layer.BlobSum)
+		if err != nil {
+			return err
+		}
+		content, err := ioutil.ReadAll(reader)
+		if err != nil {
+			return err
+		}
+		ioutil.WriteFile(layer.BlobSum.Hex()+".tgz", content, 0644)
+		reader.Close()
+	}
 	return nil
 }
